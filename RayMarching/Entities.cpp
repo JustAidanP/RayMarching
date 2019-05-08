03cbad0ca7a88d909fde9acb15e49e4c7b9d9c21
Aidan Price <justaidanp@gmail.com>
2019-05-08 21:02:42 +0100

    Added wall functionality and camera functionality

--- RayMarching/Entities.cpp
+++ RayMarching/Entities.cpp
@@ -2,10 +2,13 @@
 #include "Vectors.h"
 
 //------Entities::Wall------
-Entities2D::Wall::Wall() {}
-
-//------Entities::Camera------a
-Entities2D::Camera::Camera(Vectors::Vec2 _position, float _angle) {
-	this->position = _position;
-	this->angle = _angle;
+Entities2D::Wall::Wall(Vectors::Vec2 _position, Vectors::Vec2 _scale) { this->position = _position; this->scale = _scale; }
+float Entities2D::Wall::sdfSquared(Vectors::Vec2 &point) {
+	float dx = fmin(fmax(point.x, this->position.x), this->position.x + this->scale.x);
+	float dy = fmin(fmax(point.y, this->position.y), this->position.y + this->scale.y);
+	return powf(dx - point.x, 2) + powf(dy - point.y, 2);
 }
+
+//------Entities::Camera------
+Entities2D::Camera::Camera(Vectors::Vec2 _position, float _angle) { this->position = _position; this->angle = _angle; }
+Vectors::Vec2 Entities2D::Camera::getDirectionVec() { return Vectors::Vec2(this->angle); }
--- RayMarching/Entities.h
+++ RayMarching/Entities.h
@@ -5,10 +5,19 @@
 namespace Entities2D {
 	class Wall {
 		//------Variables/Constants------
+		public:
+			//Stores the position of the wall
+			Vectors::Vec2 position;
+			//Stores the scale of the wall as a 2-dimensional vector
+			Vectors::Vec2 scale;
 		//------Constructors------
 		public:
-			Wall();
+			Wall() {}
+			Wall(Vectors::Vec2 _position, Vectors::Vec2 _scale);
 		//------Procedures/Functions------
+		public:
+			//Returns the shortest squared distance between a point and the wall
+			float sdfSquared(Vectors::Vec2 &point);
 	};
 	//Defines a 2D camera
 	class Camera {
@@ -24,5 +33,8 @@ namespace Entities2D {
 			Camera() {}
 			Camera(Vectors::Vec2 _position, float _angle);
 		//------Procedures/Functions------
+		public:
+			//Returns the direction vector
+			Vectors::Vec2 getDirectionVec();
 	};
 }
